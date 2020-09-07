a239ab2802be7be7cfd847454dc4948f769d6a93
WooEH <soubyt5292@gmail.com>
2020-09-08 02:33:11 +0900

    java 연습2

--- JavaLearn/src/Quiz0201_17011252.java
+++ JavaLearn/src/Quiz0201_17011252.java
@@ -5,27 +5,24 @@
 //	public static void main(String[] args) {
 //		System.out.println("이름: 우은혁, 학번: 17011252");
 //		
+//		System.out.println("사는 도시, 나이, 체중, 독신 여부를 빈칸으로 분리하여 입력해주세요.");
 //		int data_count = 0;
 //		Scanner scanner = new Scanner(System.in);
 //		
-//		System.out.print("사는 도시를 입력해주세요: ");
 //		String city = scanner.next();
-//		System.out.println("도시는 " + city + ",");
+//		System.out.print("도시는 " + city + ",");
 //		data_count++;
 //		
-//		System.out.print("나이를 입력해주세요: ");
 //		int age = scanner.nextInt();
-//		System.out.println("나이는 " + age + "살, ");
+//		System.out.print("나이는 " + age + "살, ");
 //		data_count++;
 //		
-//		System.out.print("체중을 입력해주세요: ");
 //		double weight = scanner.nextDouble();
-//		System.out.println("체중 " + weight + "kg, ");
+//		System.out.print("체중 " + weight + "kg, ");
 //		data_count++;
 //		
-//		System.out.print("독신 여부를 입력해주세요: ");
 //		boolean single = scanner.nextBoolean();
-//		System.out.println("독신 여부는 " + single + "입니다. ");
+//		System.out.print("독신 여부는 " + single + "입니다. ");
 //		data_count++;
 //		
 //		System.out.print("입력된 데이터의 수는 " + data_count + "개 입니다.");
