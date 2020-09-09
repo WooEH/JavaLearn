160afecea4cfe69337dd29298700c875ac04e653
WooEH <soubyt5292@gmail.com>
2020-09-10 02:00:07 +0900

    업데이

---
+++ JavaLearn/src/Ex02_07.java
@@ -0,0 +1,20 @@
+//
+//public class Ex02_07 {
+//
+//	public static void main(String[] args) {
+//		//비교 연
+//		System.out.println('a'>'b');
+//		System.out.println(3>=2);
+//		System.out.println(-1<0);
+//		System.out.println(3.45<=2);
+//		System.out.println(3==2);
+//		System.out.println(3!=2);
+//		System.out.println(!(3!=2));
+//		
+//		//비교 연산과 논리 연산의 복합
+//		System.out.println((3>2)&&(3>4));
+//		System.out.println((3!=2)||(-1>0));
+//		System.out.println((3!=2)^(-1>0));
+//	}
+//
+//}
