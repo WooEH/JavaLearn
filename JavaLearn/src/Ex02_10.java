3a0f3d11111d5febec72ea7b5c86603517d07d9e
WooEH <soubyt5292@gmail.com>
2020-09-12 01:45:52 +0900

    ㅋㅋ

---
+++ JavaLearn/src/Ex02_10.java
@@ -0,0 +1,19 @@
+//import java.util.Scanner;
+//
+//public class Ex02_10 {
+//
+//	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		System.out.print("점수를 입력하시오: ");
+//		int score = scanner.nextInt();
+//		
+//		if(score>=80) {
+//			System.out.println("합격 축축");
+//		}
+//		
+//		scanner.close();
+//
+//	}
+//
+//}
